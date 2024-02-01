35a1793ee135545cac5cf449f4059762bda1addb
YinCho <99512912+YinCho@users.noreply.github.com>
2024-01-31 21:13:07 -0800

    BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -2,8 +2,8 @@ public class BinarySearch {
     public static int binarySearch(int[] arr, int start, int end, int key){
         int mid = (start+end)/2; 
         if (arr[mid] == key) return mid; 
-        if (arr[mid] > key) binarySearch(arr, start, mid, key); 
-        if (arr[mid] < key) binarySearch(arr, mid, end, key);
+        if (arr[mid] > key) binarySearch(arr, start, mid-1, key); 
+        if (arr[mid] < key) binarySearch(arr, mid+1, end, key);
         return -1; 
     }
     public static void main(String[] args) {
